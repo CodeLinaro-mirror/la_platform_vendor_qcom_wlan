02783136884888256be7bd030ff410ae9eb00046
Ajit Vaishya <avaishya@codeaurora.org>
2021-10-22 08:57:54 -0700

    Disabled wifi-aware and rtt service for wear target
    Disable Wifi-Aware and RTT service based on wear flags
    i.e TARGET_SUPPORTS_WEARABLES, which is enabled for
    both target LAW and LW wear target.
    
    Change-Id: I5ae02b4f11aeecb151b1e1538eedf95d9fc33636
    CRs-Fixed: 3058784

--- monaco/wlan.mk
+++ monaco/wlan.mk
@@ -10,8 +10,10 @@ PRODUCT_PACKAGES += $(WLAN_CHIPSET)_wlan.ko
 PRODUCT_PACKAGES += wifilearner
 PRODUCT_PACKAGES += $(WPA)
 
+ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
 #Enable WIFI AWARE FEATURE
 WIFI_HIDL_FEATURE_AWARE := true
+endif
 
 PRODUCT_COPY_FILES += \
 	device/qcom/wlan/monaco/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
@@ -19,7 +21,7 @@ PRODUCT_COPY_FILES += \
 	device/qcom/wlan/monaco/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
 	device/qcom/wlan/monaco/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf
 
-ifneq ($(TARGET_SUPPORTS_WEAR_OS),true)
+ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
 	frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
