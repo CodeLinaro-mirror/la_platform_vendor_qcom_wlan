ec75827bc1397aba03531fc762b1e31a0fd0666b
Chenming Huang <quic_chenhuan@quicinc.com>
2022-08-30 11:25:02 +0800

    wifi: Add Dual AP support
    Add Dual AP support.
    
    CRs-Fixed: 3278441
    Change-Id: I76c261caca611ff2be8e99cecc3abca6fbc3b6ef

--- msmnile_au/wlan.mk
+++ msmnile_au/wlan.mk
@@ -51,7 +51,8 @@ PRODUCT_PACKAGES += ctrlapp_dut
 # AOSP: interface combinations
 WIFI_HAL_INTERFACE_COMBINATIONS := {{{STA}, 1}, {{AP}, 1}, {{P2P}, 1}},\
                                    {{{STA}, 1}, {{NAN}, 1}}, \
-                                   {{{STA}, 2}, {{AP}, 1}}
+                                   {{{STA}, 2}, {{AP}, 2}}, \
+                                   {{{STA}, 2}, {{AP_BRIDGED}, 1}}
 
 # Enable vendor properties.
 PRODUCT_PROPERTY_OVERRIDES += \
