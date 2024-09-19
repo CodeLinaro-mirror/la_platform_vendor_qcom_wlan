cf3c95eb241f0a1cd5154956c7bd1fa47d21050a
Yu Ouyang <quic_yuo@quicinc.com>
2024-09-19 17:02:00 +0800

    wlan: trinket: Enable application ctrlapp_dut
    Build ctrlapp_dut and install it.
    
    Change-Id: Ieabda814e73e6747f158d189a4ee8f4993b56674
    CRs-Fixed: 3927821

--- trinket/wlan.mk
+++ trinket/wlan.mk
@@ -5,6 +5,7 @@ WPA := wpa_cli
 
 PRODUCT_PACKAGES += $(WLAN_CHIPSET)_wlan.ko
 PRODUCT_PACKAGES += wifilearner
+PRODUCT_PACKAGES += ctrlapp_dut
 PRODUCT_PACKAGES += $(WPA)
 
 #Enable WIFI AWARE FEATURE
