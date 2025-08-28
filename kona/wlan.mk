5d4d4e6b7508e6230c987b5c48855906fcc9deb5
gaolez <gaolez@qti.qualcomm.com>
2025-08-28 18:06:32 +0800

    kona: Add cfr vendor .xml file
    Addng cfr vendor .xml file, the vendor_cmd_tool will accroding this
    file to generate netlink vendor command to enable cfr capture.
    
    Change-Id: Icd1dd7d0520dc79ed8e07535b9f18a3300fc19b8
    CRs-Fixed: 4270991

--- kona/wlan.mk
+++ kona/wlan.mk
@@ -20,6 +20,7 @@ PRODUCT_COPY_FILES += \
 			device/qcom/wlan/kona/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 			device/qcom/wlan/kona/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
 			device/qcom/wlan/kona/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf \
+			device/qcom/wlan/kona/vendor_cmd_cfr.xml:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/vendor_cmd_cfr.xml \
 			frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
 			frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
 			frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
