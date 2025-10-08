5abb81bcf072df75e42f22022de9b96df4b0b477
Ravindra Konda <konda@qti.qualcomm.com>
2025-10-08 00:07:50 -0700

    Shipping below bins in HY22 builds. - athdiag - ftmdaemon - wdsdaemon - cnss_diag - pktlogconf - cnss-daemon - vendor_cmd_tool - hal_proxy_daemon - init.vendor.wlan.rc
    Change-Id: Ic7a56d0fdf458c3413bcc073e2d69a48a1dc8253

--- canoe/wlan.mk
+++ canoe/wlan.mk
@@ -109,10 +109,27 @@ WLAN_MODULES_VENDOR += hal_proxy_daemon
 WLAN_MODULES_VENDOR += spectraltool
 WLAN_MODULES_VENDOR += pktlogconf
 endif
+
 ifeq ($(wildcard $(QCPATH)/wlan/utils),)
 $(call soong_config_set,qtiwlan,hy22,true)
+WLAN_MODULES_VENDOR += athdiag
+WLAN_MODULES_VENDOR += pktlogconf
+WLAN_MODULES_VENDOR += hal_proxy_daemon
 WLAN_MODULES_VENDOR += init.vendor.wlan.rc
 endif
+
+ifeq ($(wildcard $(QCPATH)/ftm),)
+$(call soong_config_set,qtiwlan,hy22,true)
+WLAN_MODULES_VENDOR += ftmdaemon
+WLAN_MODULES_VENDOR += wdsdaemon
+endif
+
+ifeq ($(wildcard $(QCPATH)/wlan/common-tools),)
+$(call soong_config_set,qtiwlan,hy22,true)
+WLAN_MODULES_VENDOR += vendor_cmd_tool
+WLAN_MODULES_VENDOR += cnss_diag
+endif
+
 ifneq ($(wildcard $(QCPATH)/wlan/oem/oem-ss),)
 WLAN_MODULES_VENDOR += libwpa_drv_oem
 endif
